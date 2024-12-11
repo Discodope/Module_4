8fb824194ddfead2ed8d390658d829bbd1cee3cb
Даниил <daniil.lebedinsky@gmail.com>
2024-12-11 19:37:26 +0300

    ДЗ №1

--- true_math.py
+++ true_math.py
@@ -3,8 +3,6 @@ def div(first , second):
     if second != 0:
         sum = first / second
         print('Ответ : ', sum)
+        return sum
     elif second == 0:
         print('Ответ', float(math.inf))
-
-div(5, 0)
-div(6,2)
