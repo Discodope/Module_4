8794c6a5d3c08d2413b20759a3ef45371b1a86ef
Даниил <daniil.lebedinsky@gmail.com>
2024-12-11 19:37:11 +0300

    ДЗ №1

--- fake_math.py
+++ fake_math.py
@@ -1,9 +1,9 @@
 def div(first, second):
+    sum = []
     if second != 0:
         sum = first / second
         print('Ответ : ', sum)
+        return sum
     elif second == 0:
         print('Ошибка!')
 
-div(5, 0)
-div(6,2)
